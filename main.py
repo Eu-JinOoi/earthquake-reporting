8004e50dddbe54bbc9dddee6ae036ee577f62fe9
Eu-Jin Ooi <Ooi@Eu-J.in>
2016-07-08 13:16:34 -0700

    Modified to use curses.wrapper which more gracefully handles unexpected exits of curses. Kept CTRL+C behavior to gracefully exit from that condition. Code cleanup

--- main.py
+++ main.py
@@ -10,23 +10,56 @@ import curses
 from colorz import colorz
 from earthquake import earthquake
 
-def consoleLog(geoJsonData):
-	print(geoJsonData['metadata']['count']);
 def signal_handler(signal, frame):
 	#Want to clean up CTRL+C action
 	curses.reset_shell_mode();
-	print();
+	print("\nJust because of that, the big one is going to hit now...");
 	sys.exit(0);
-def main():
-	#Curses
-	scr = curses.initscr();
+	
+def run(scr,debug,dataSet):
 	curses.start_color();
-	#curses.noecho();
-	#curses.cbreak();
-	#scr.keypad(True);
-	#scr.keypad(False);	
-	#curses.echo();
-	#curses.nocbreak;
+	#Should have args that select which feed to use. 
+	#API Doc http://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+	url='';
+	if(dataSet == 'hour'):
+		#All Hour
+		url='http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson';
+	elif(dataSet == 'day'):
+		#All Day
+		url='http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
+	elif(dataSet == 'week'):
+		#All Week
+		url='http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
+	elif(dataSet == 'month'):
+		#All Month
+		url='http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson';
+
+	#Pretty Print
+	pp = pprint.PrettyPrinter(indent=4)
+
+	while(1):
+		r = requests.get(url);
+		data = r.json()
+		count = 0;
+		
+		curses.update_lines_cols();
+		screenSize=scr.getmaxyx();
+		scr.resize(screenSize[0],screenSize[1]);
+		maxQuakes=screenSize[0]-1;
+		for quake in data['features']:
+			eq = earthquake(quake)	
+			eq.curseQuake(scr,count);
+			count+=1;
+			if(count >=maxQuakes):
+				break;
+		scr.addstr(count,0,"Last update: " + str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')));
+		scr.refresh();
+		scr.erase();
+		#Time between steps
+		time.sleep(60);
+
+#_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+#Our Main Program
 
 #Handle CTRL+C
 signal.signal(signal.SIGINT, signal_handler);
@@ -43,48 +76,6 @@ if '?' in sys.argv:
 	print ("?		Displays this menu");
 	exit();	
 
-#Should have args that select which feed to use. 
-#API Doc http://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
-#All Hour
-#url='http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson';
-#All Day
-url='http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
-#All Week
-#url='http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
-#All Month
-#url='http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson';
+dataSet='day';
 
-#Pretty Print
-pp = pprint.PrettyPrinter(indent=4)
-
-#main();
-scr = curses.initscr();
-while(1):
-	r = requests.get(url);
-	data = r.json()
-	if(debug == True):
-		#Pretty Print
-		pp = pprint.PrettyPrinter(indent=4)
-		pp.pprint(data);
-	count = 0;
-	
-	curses.update_lines_cols();
-	screenSize=scr.getmaxyx();
-	scr.resize(screenSize[0],screenSize[1]);
-	maxQuakes=screenSize[0]-1;
-
-	for quake in data['features']:
-		if(debug == True):
-			print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-			pp.pprint(quake);
-		eq = earthquake(quake)	
-		eq.curseQuake(scr,count);
-		count+=1;
-		if(count >=maxQuakes):
-			break;
-	scr.addstr(count,0,"Last update: " + str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')));
-	scr.refresh();
-	scr.erase();
-	#Time between steps
-	time.sleep(60);
-
+curses.wrapper(run,debug,dataSet);
